7a67bb30f649dc6ea03e332cef881d60a30bec75
Sire65 <ha-joko@web.de>
2026-08-24 07:17:31 +0200

    Add shared runtime flags for MarktKasse suite

---
+++ markt-kasse-suite/shared/runtime-flags.js
@@ -0,0 +1,5 @@
+/* Zentraler Umschalter für Hinweise, die nur während der Erprobung sichtbar sein dürfen. */
+window.KC_RUNTIME_FLAGS=Object.freeze({
+  testPhaseToolGuidance:true,
+  candidateTestAccess:true
+});
